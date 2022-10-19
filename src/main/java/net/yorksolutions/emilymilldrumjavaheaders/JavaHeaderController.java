29121ee0a34b7a0fc66e21361e9490b55ebf76bb
Emily <emilldr>
2022-10-19 15:02:23 -0500

    ipaddress, date, headers ugly but functioning

--- src/main/java/net/yorksolutions/emilymilldrumjavaheaders/JavaHeaderController.java
+++ src/main/java/net/yorksolutions/emilymilldrumjavaheaders/JavaHeaderController.java
@@ -4,15 +4,34 @@ package net.yorksolutions.emilymilldrumjavaheaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.Map;
+
 @RestController
 public class JavaHeaderController {
-
-    public JavaHeaderController(){
-
+    private final JavaHeaderService javaHeaderService;
+    public JavaHeaderController(JavaHeaderService javaHeaderService) {
+        //this.request = request;
+        this.javaHeaderService = javaHeaderService;
     }
 
     @GetMapping("/headers")
-    public String generateName(){
-        return "test";
+    public Map<String, String> generateName() {
+        //return this.javaHeaderService.ip();
+        return this.javaHeaderService.getRequestHeadersInMap();
     }
+
+    @GetMapping("/ipaddress")
+    public String ipAd() {
+        //return this.javaHeaderService.ip();
+        return this.javaHeaderService.getClientIp();
+    }
+
+    @GetMapping("/date")
+    public String date() {
+        //return this.javaHeaderService.ip()
+        return this.javaHeaderService.date();
+    }
+
+
 }
--- src/main/java/net/yorksolutions/emilymilldrumjavaheaders/JavaHeaderService.java
+++ src/main/java/net/yorksolutions/emilymilldrumjavaheaders/JavaHeaderService.java
@@ -1,4 +1,85 @@
 package net.yorksolutions.emilymilldrumjavaheaders;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
+
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class JavaHeaderService {
+    private final String LOCALHOST_IPV4 = "127.0.0.1";
+    private final String LOCALHOST_IPV6 = "0:0:0:0:0:0:0:1";
+    private static HttpServletRequest request;
+    private static String remoteAddr = "";
+    public JavaHeaderService(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    public static String ip(){
+        if (request != null){
+            remoteAddr = request.getHeader("X-FORWARDED-FOR");
+            if (remoteAddr == null || "".equals(remoteAddr)) {
+                remoteAddr = request.getRemoteAddr();
+            }
+        }
+        return remoteAddr;
+    }
+
+    public Map<String, String> getRequestHeadersInMap() {
+
+        Map<String, String> result = new HashMap<>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            result.put(key, value);
+        }
+
+        return result;
+    }
+
+    public String getClientIp() {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if(StringUtils.hasLength(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+
+        if(StringUtils.hasLength(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if(StringUtils.hasLength(ipAddress) || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+            if(LOCALHOST_IPV4.equals(ipAddress) || LOCALHOST_IPV6.equals(ipAddress)) {
+                try {
+                    InetAddress inetAddress = InetAddress.getLocalHost();
+                    ipAddress = inetAddress.getHostAddress();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if(!StringUtils.hasLength(ipAddress)
+                && ipAddress.length() > 15
+                && ipAddress.indexOf(",") > 0) {
+            ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+        }
+
+        return ipAddress;
+    }
+
+
+    public String date(){
+        Date date = new Date();
+        return date.toString();
+    }
 }
